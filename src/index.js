d054b72cb5fa18857596ccc06d59a978f680f09b
Cherkaoui El Wafi <elwafi.cherkaoui@gmail.com>
2024-09-30 23:34:37 +0100

    Add event listeners to all buttons

--- src/index.js
+++ src/index.js
@@ -1,8 +1,36 @@
 import './styles.css'
 import Dom from './dom-module'
 
+let addTaskBtn = document.querySelector('#add_task')
+let addProjectBtn = document.querySelector('#add_project')
+
+let allProjectsBtn = document.querySelector('#all_projects')
+let allTodosBtn = document.querySelector('#all_tasks')
+
+
+addTaskBtn.addEventListener('click', ()=>{
+    Dom.refreshDetailsCont()
+    Dom.todoForm()
+})
+addProjectBtn.addEventListener('click', ()=>{
+    Dom.refreshDetailsCont()
+    Dom.projectForm()
+})
+
+allProjectsBtn.addEventListener('click', ()=>{
+    Dom.refreshDetailsCont()
+    Dom.showAllProjects()
+
+})
+allTodosBtn.addEventListener('click', ()=>{
+    Dom.refreshDetailsCont()
+
+    Dom.showAllTodos()
+    console.log(Dom.projects);
+    
+})
 // Dom.projectForm()
-Dom.todoForm()
+// Dom.todoForm()
 
 // console.log(Dom);
 
